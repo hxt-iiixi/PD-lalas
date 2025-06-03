95423889e43b01ac1e45bffa1271112a004982e9
hxt-iiixi <kede.camagay.up@gmail.com>
2025-06-03 14:20:51 +0800

    Refactor: Replace hardcoded assets with Vite-managed @vite directive

--- web/resources/views/layouts/app.blade.php
+++ web/resources/views/layouts/app.blade.php
@@ -5,9 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
 </head>
 <body>
     

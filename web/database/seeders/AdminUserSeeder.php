a2ed3670e3e205cb31e61dc502efe8eb2a9a6de9
hxt-iiixi <kede.camagay.up@gmail.com>
2025-06-19 22:55:21 +0800

    gdf

--- web/database/seeders/AdminUserSeeder.php
+++ web/database/seeders/AdminUserSeeder.php
@@ -20,5 +20,15 @@ class AdminUserSeeder extends Seeder
                 'is_approved' => true, // if your schema includes this
             ]
         );
+        User::updateOrCreate(
+            ['email' => 'phai@gmail.com'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('zachzeth'), // You can change this securely later
+                'is_admin' => true, // assumes you have an `is_admin` boolean field
+                'is_active' => true, // if you have an `is_active` field
+                'is_approved' => true, // if your schema includes this
+            ]
+        );
     }
 }
